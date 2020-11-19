d3b65fcf23dd86299261f65d10eb7a2a7975f1da
HyperFuryXD <69196456+HyperFuryXD@users.noreply.github.com>
2020-11-19 19:30:47 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(1600,400);
  bullet = createSprite(100, 200, 50, 20);
  bullet.shapeColor = "white";
-
+wall = createSprite(1200,200,thickness,height/2);
  speed = random(223,321);
  weight = random(30,52);
  thickness = random(22,83);
